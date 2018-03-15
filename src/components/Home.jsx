2a52338c935de376a9b3e7548f9ef96f57cba11f
yasirrose <yasirrose@hotmail.com>
2018-03-15 23:47:20 +0500

    Getting Data from API for login and Banners

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 // import { Link } from 'react-router-dom';
+import axios from 'axios';
 import {Grid, Row,Carousel} from 'react-bootstrap';
 import ProdutosInspirados from './home-page/ProdutosInspirados.jsx';
 import Produtosemdestaque from './home-page/Produtosemdestaque.jsx';
@@ -9,25 +10,36 @@ import Servicos from './home-page/Servicos.jsx';
 import Appstore from './home-page/Appstore.jsx';
 import './Home.css';
 export default class Home extends Component {
+  constructor() {
+    super();
+    this.state = {
+      banner_list :[],
+    };
+  }
+  componentDidMount() {
+    axios.get(`http://visualtotal.com.br/api/banners`,
+    {
+      headers: {
+        apikey: 'fc5d0fec-6d36-43a4-8ad7-97f9633dc84a',
+      }
+    })
+    .then(res => {
+      const banner = res.data.object.map(obj => obj.image);
+      const carousel_text= <Carousel.Caption>
+              <h1>Hair Dresser</h1>
+              <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam </p>
+            </Carousel.Caption>; 
+      const banner_list = banner.map(obj => <Carousel.Item><img width={900} height={445} alt="banner" src={obj.image_url} />{carousel_text}</Carousel.Item>);
+      this.setState({banner_list});
+      //console.log(res.data.object[0].image.image_url);
+    });
+  }
   render() {
     return (
       <div className="homePage">
         <div className="banner">
         <Carousel>
-          <Carousel.Item>
-            <img width={900} height={445} alt="banner" src="assets/images/banner.png" />
-            <Carousel.Caption>
-              <h1>Hair Dresser</h1>
-              <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam </p>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
-            <img width={900} height={445} alt="banner" src="assets/images/banner.png" />
-            <Carousel.Caption>
-              <h1>Hair Dresser</h1>
-              <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam </p>
-            </Carousel.Caption>
-          </Carousel.Item>
+          {this.state.banner_list}
           </Carousel>
         </div>
         <Grid className="page-content">
--- src/components/forms/loginTooltip.jsx
+++ src/components/forms/loginTooltip.jsx
@@ -1,48 +1,107 @@
 import React, { Component } from 'react'
+import axios from 'axios';
 import { ControlLabel,FormGroup,FormControl,Button} from 'react-bootstrap';
+
 export default class Loginform extends Component{
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+      msg: '',
+    };
+  }
+  onChange = (e) => {
+    // Because we named the inputs to match their corresponding values in state, it's
+    // super easy to update the state
+    const state = this.state
+    state[e.target.name] = e.target.value;
+    this.setState(state);
+  }
+
+  onSubmit = (e) => {
+    e.preventDefault();
+    const { email, password } = this.state;
+    axios.post(`http://visualtotal.com.br/login?email=${email}&password=${password}`)
+    .then((res) => {
+      this.setState({ msg: <div className="alert alert-success">Login Successfuly</div>});
+      console.log(res.data.apikey.key);
+    })
+    .catch((error) => {
+      if (error.response) {
+        // The request was made and the server responded with a status code
+        console.log(error.response.data.message);
+        console.log(error.response.status);
+        console.log(error.response.headers);
+        const msg = error.response.data.message;
+        this.setState({ msg: <div className="alert alert-danger">{msg}</div>});
+      } else if (error.request) {
+        // The request was made but no response was received
+        // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+        // http.ClientRequest in node.js
+        console.log(error.request);
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        console.log('Error', error.message);
+      }
+      console.log(error.config);
+    })
+    // .then(function(res) {
+    //   
+    // })
+  }
+
   render() {
+    const { email, password} = this.state;
     return (
-      <form className="login-form">
-        <div className="arowForToltip"></div>
-        <FormGroup
-          controlId="email"
-        >
-          <ControlLabel>E-mail</ControlLabel>
-          <FormControl
-            type="email"
-            placeholder="igor.anjos@wemind.com.br"
-          />
-        </FormGroup>
-        <FormGroup
-          controlId="pswrd"
-        >
-          <ControlLabel>Senha</ControlLabel>
-          <FormControl
-            type="password"
-            placeholder="**********"
-            autoComplete=""
-          />
-          <div className="text-right form-link">
-          <a href="jsx-a11y/href-no-hash">Esqueci minha senha</a>
+      <div className="login-form-wrap">
+      <div>{this.state.msg}</div>
+        <form className="login-form" onSubmit={this.onSubmit}>
+          <div className="arowForToltip"></div>
+          <FormGroup
+            controlId="email"
+          >
+            <ControlLabel>E-mail</ControlLabel>
+            <FormControl
+              type="email"
+              placeholder="igor.anjos@wemind.com.br"
+              name="email" 
+              value={email} onChange={this.onChange}
+            />
+          </FormGroup>
+          <FormGroup
+            controlId="pswrd"
+          >
+            <ControlLabel>Senha</ControlLabel>
+            <FormControl
+              type="password"
+              placeholder="**********"
+              autoComplete=""
+              name="password" 
+              value={password} onChange={this.onChange}
+            />
+            <div className="text-right form-link">
+            <a href="jsx-a11y/href-no-hash">Esqueci minha senha</a>
+            </div>
+          </FormGroup>
+          <Button type="submit" bsStyle="success" className="loginBtn">Login</Button>
+          <div className="text-center form-link">
+            <a href="jsx-a11y/href-no-hash">Não registrado ainda? Clique aqui</a>
           </div>
-        </FormGroup>
-        <Button type="submit" bsStyle="success" className="loginBtn">Login</Button>
-        <div className="text-center form-link">
-          <a href="jsx-a11y/href-no-hash">Não registrado ainda? Clique aqui</a>
-        </div>
-        <div className="social-logins">
-          <FormGroup>
-            <Button type="submit" bsStyle="primary" className="loginBtnfb">Entrar com o Facebook</Button>
-          </FormGroup>
-          <FormGroup>
-            <Button type="submit" bsStyle="danger" className="loginBtngp">Entrar com o Google +</Button>
-          </FormGroup>
-          <FormGroup>
-            <Button type="submit" bsStyle="info" className="loginBtntw">Entrar com o Twitter</Button>
-          </FormGroup>
-        </div>
-      </form>
+          <div className="social-logins">
+            <FormGroup>
+              <Button type="submit" bsStyle="primary" className="loginBtnfb">Entrar com o Facebook</Button>
+            </FormGroup>
+            <FormGroup>
+              <Button type="submit" bsStyle="danger" className="loginBtngp">Entrar com o Google +</Button>
+            </FormGroup>
+            <FormGroup>
+              <Button type="submit" bsStyle="info" className="loginBtntw">Entrar com o Twitter</Button>
+            </FormGroup>
+          </div>
+        </form>
+      </div>
+
     );
   }
 }
