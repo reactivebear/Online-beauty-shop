00e87af1765e04c6c86ec4fe184842f8558d8502
Caio Pelichek Gonçalves <cpelichek@gmail.com>
2018-04-13 11:56:24 -0300

    Adjusted download button at the right side of the navbar

--- src/components/templates/CustomNavbar.jsx
+++ src/components/templates/CustomNavbar.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Navbar, Nav, NavItem, Row, Col,FormGroup,FormControl,Button,Image,DropdownButton,MenuItem,Glyphicon} from 'react-bootstrap';
+import { Grid, Navbar, Nav, NavItem, Row, Col,FormGroup,FormControl,Button,Image,DropdownButton,MenuItem,Glyphicon} from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import './CustomNavbar.css';
 import Headerlogin from '../tooltips/Headerlogin.jsx';
@@ -20,87 +20,90 @@ export default class CustomNavbar extends Component {
           </Navbar.Header>
           <Nav pullRight className="download-app-button">
             <NavItem eventKey={1} href="#">
-              <Button type="button" className="btn btn-sm btn-success"><i className="fa fa-mobile" aria-hidden="false"></i> Baixe grátis o app do Visual Total</Button>
-              </NavItem>
+              <Button type="button" className="btn btn-sm btn-success btn-app"><i className="fa fa-mobile" aria-hidden="false"></i> Baixe grátis o app do Visual Total</Button>
+            </NavItem>
           </Nav>
           <Navbar.Collapse>
-            <Row className="navbar-ul">
-              <Nav  className="main-ul">
-                <NavItem eventKey={1} componentClass={Link} href="/" to="/">
-                  Hairtylist & Makeup
-                </NavItem>
-                <NavItem eventKey={2} componentClass={Link} href="/about" to="/about">
-                  Barber
-                </NavItem>
-                <NavItem eventKey={3} componentClass={Link} href="/news" to="/news">
-                  Depilation
-                </NavItem>
-                <NavItem eventKey={3} componentClass={Link} href="/news" to="/news">
-                  Esthetic Clinics
-                </NavItem>
-                <NavItem eventKey={3} componentClass={Link} href="/news" to="/news">
-                  Spa & Massage
-                </NavItem>
-              </Nav>
-            </Row>
-            <Row className="show-grid">
-              <Col xs={12} md={9} className="navbar-left-section">
-                <div className="searchbar hidden-xs">
-                  <DropdownButton id="navdrop-dropdown" title="Todos" className="searchbarDropDown">
-                     <MenuItem eventKey="1">Action</MenuItem>
-                     <MenuItem eventKey="2">Another action</MenuItem>
-                     <MenuItem eventKey="3" active>
-                       Active Item
-                     </MenuItem>
-                     <MenuItem divider />
-                     <MenuItem eventKey="4">Separated link</MenuItem>
-                   </DropdownButton>
-                  <Navbar.Form pullLeft>
-                    <FormGroup>
-                      <FormControl type="text" placeholder="Buscar por produtos e serviços" />
-                    </FormGroup>{' '}
-                    <Button type="submit" className="btn btn-success"><Glyphicon glyph="search" /></Button>
-                  </Navbar.Form>
-                </div>
-              </Col>
-              
-              <NavbarRightSection />
+            <Grid>
+              <Row className="navbar-ul">
+                <Nav  className="main-ul">
+                  <NavItem eventKey={1} componentClass={Link} href="/" to="/">
+                    Hairtylist & Makeup
+                  </NavItem>
+                  <NavItem eventKey={2} componentClass={Link} href="/about" to="/about">
+                    Barber
+                  </NavItem>
+                  <NavItem eventKey={3} componentClass={Link} href="/news" to="/news">
+                    Depilation
+                  </NavItem>
+                  <NavItem eventKey={3} componentClass={Link} href="/news" to="/news">
+                    Esthetic Clinics
+                  </NavItem>
+                  <NavItem eventKey={3} componentClass={Link} href="/news" to="/news">
+                    Spa & Massage
+                  </NavItem>
+                </Nav>
+              </Row>
+              <Row className="show-grid">
+                <Col xs={12} md={9} className="navbar-left-section">
+                  <div className="searchbar hidden-xs">
+                    <DropdownButton id="navdrop-dropdown" title="Todos" className="searchbarDropDown" noCaret>
+                      <MenuItem eventKey="1">Action</MenuItem>
+                      <MenuItem eventKey="2">Another action</MenuItem>
+                      <MenuItem eventKey="3" active>
+                        Active Item
+                      </MenuItem>
+                      <MenuItem divider />
+                      <MenuItem eventKey="4">Separated link</MenuItem>
+                    </DropdownButton>
+                    <Navbar.Form pullLeft>
+                      <FormGroup>
+                        <FormControl type="text" placeholder="Buscar por produtos e serviços" />
+                      </FormGroup>{' '}
+                      <Button type="submit" className="btn btn-success"><Glyphicon glyph="search" /></Button>
+                    </Navbar.Form>
+                  </div>
+                </Col>
+                
+                <NavbarRightSection />
 
-              {/* <Col xs={6} md={3} className="navbar-right-section hidden-xs">
-                <Row className="show-grid">
-                  <Col xs={1}>
-                    <div className="navbar-side location-div nbs">
-                      <Adicionar />
-                    </div>
-                  </Col>
-                  <Col xs={4}>
-                    <div className="navbar-side location-div nbs">
-                      <p>Enviar para</p>
-                      <p><strong>Bela Vista 01329900</strong></p>
-                    </div>
-                  </Col>
-                  <Col xs={1} md={1}>
-                    <div className="navbar-side-cart nbs">
-                      <Carttooltip />
-                    </div>
-                  </Col>
-                  <Col xs={5} md={5}>
-                    <div className="navbar-side-cart nbs">
-                      <div className="navbar-side">
-                        <p>Meu</p>
-                        <p><strong>Carrinho</strong></p>
+                {/* <Col xs={6} md={4} className="navbar-right-section hidden-xs">
+                  <Row className="show-grid">
+                    <Col xs={1}>
+                      <div className="navbar-side location-div nbs">
+                        <Adicionar />
                       </div>
-                    </div>
-                  </Col>
-                  <Col xs={7} md={7}>
-                    <div className="navbar-side nbs">
-                      <p>Bem vindo</p>
-                      <Headerlogin />
-                    </div>
-                  </Col>
-                </Row>
-              </Col> */}
-            </Row>
+                    </Col>
+                    <Col xs={4}>
+                      <div className="navbar-side location-div nbs">
+                        <p>Enviar para</p>
+                        <p><strong>Bela Vista 01329900</strong></p>
+                      </div>
+                    </Col>
+                    <Col xs={1} md={1}>
+                      <div className="navbar-side-cart nbs">
+                        <Carttooltip />
+                      </div>
+                    </Col>
+                    <Col xs={5} md={5}>
+                      <div className="navbar-side-cart nbs">
+                        <div className="navbar-side">
+                          <p>Meu</p>
+                          <p><strong>Carrinho</strong></p>
+                        </div>
+                      </div>
+                    </Col>
+                    <Col xs={7} md={7}>
+                      <div className="navbar-side nbs">
+                        <p>Bem vindo</p>
+                        <Headerlogin />
+                      </div>
+                    </Col>
+                  </Row>
+                </Col> */}
+
+              </Row>
+            </Grid>
           </Navbar.Collapse>
 
 
@@ -122,45 +125,48 @@ export default class CustomNavbar extends Component {
             </Navbar.Form>
           </div>
           <div className="hidden-lg hidden-sm hidden-md sub-header">
-            <Row className="show-grid ">
+            <Grid>
+              <Row className="show-grid ">
 
-              <NavbarRightSection />
+                <NavbarRightSection />
 
-              {/* <Col xs={6} md={3} className="navbar-right-section hidden-xs">
-                <Row className="show-grid">
-                  <Col xs={1}>
-                    <div className="navbar-side location-div nbs">
-                      <Adicionar />
-                    </div>
-                  </Col>
-                  <Col xs={4}>
-                    <div className="navbar-side location-div nbs">
-                      <p>Enviar para</p>
-                      <p><strong>Bela Vista 01329900</strong></p>
-                    </div>
-                  </Col>
-                  <Col xs={1} md={1}>
-                    <div className="navbar-side-cart nbs">
-                      <Carttooltip />
-                    </div>
-                  </Col>
-                  <Col xs={5} md={5}>
-                    <div className="navbar-side-cart nbs">
-                      <div className="navbar-side">
-                        <p>Meu</p>
-                        <p><strong>Carrinho</strong></p>
+                {/* <Col xs={6} md={3} className="navbar-right-section hidden-xs">
+                  <Row className="show-grid">
+                    <Col xs={1}>
+                      <div className="navbar-side location-div nbs">
+                        <Adicionar />
                       </div>
-                    </div>
-                  </Col>
-                  <Col xs={7} md={7}>
-                    <div className="navbar-side nbs">
-                      <p>Bem vindo</p>
-                      <Headerlogin />
-                    </div>
-                  </Col>
-                </Row>
-              </Col> */}
-            </Row>
+                    </Col>
+                    <Col xs={4}>
+                      <div className="navbar-side location-div nbs">
+                        <p>Enviar para</p>
+                        <p><strong>Bela Vista 01329900</strong></p>
+                      </div>
+                    </Col>
+                    <Col xs={1} md={1}>
+                      <div className="navbar-side-cart nbs">
+                        <Carttooltip />
+                      </div>
+                    </Col>
+                    <Col xs={5} md={5}>
+                      <div className="navbar-side-cart nbs">
+                        <div className="navbar-side">
+                          <p>Meu</p>
+                          <p><strong>Carrinho</strong></p>
+                        </div>
+                      </div>
+                    </Col>
+                    <Col xs={7} md={7}>
+                      <div className="navbar-side nbs">
+                        <p>Bem vindo</p>
+                        <Headerlogin />
+                      </div>
+                    </Col>
+                  </Row>
+                </Col> */}
+
+              </Row>
+            </Grid>
           </div>
         </Navbar>
       </div>
