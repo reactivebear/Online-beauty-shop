588a342157289898981a72c7d5d7c869de7f6316
Caio Pelichek Gonçalves <cpelichek@gmail.com>
2018-05-07 17:04:54 -0300

    Replace footer content

--- src/components/templates/Footer.jsx
+++ src/components/templates/Footer.jsx
@@ -1,6 +1,9 @@
 import React, { Component } from 'react'
 // import { Link } from 'react-router-dom';
-import { Row, Col,Grid,Image} from 'react-bootstrap';
+import { Col, DropdownButton, Grid, Image, MenuItem, Row} from 'react-bootstrap';
+
+import DownloadAppButtonSmall from '../templates/DownloadAppButtonSmall.jsx';
+
 import './Footer.css';
 var FontAwesome = require('react-fontawesome');
 export default class Footer extends Component {
@@ -10,28 +13,97 @@ export default class Footer extends Component {
           <Grid>
             <Row className="content">
               <Col >
-                <div className="footerLogo">
-                  <Image src="assets/images/footerLogo.png" />
-                </div>
-                <div className="social">
-                  <div className="twiter">
-                      <a href=""><FontAwesome
-                       name='twitter'
-                     /></a>
+                <Row className="footer-top">
+                  <div className="footerLogo">
+                    <Image src="assets/images/footerLogo.png" />
                   </div>
-                  <div className="google-plus">
-                      <a href=""><FontAwesome
-                       name='google-plus'
-                     /></a>
-                  </div>
-                  <div className="facebook">
-                    <a href=""><FontAwesome
-                       name='facebook'
-                     /></a>
-                  </div>
-                </div>
+
+                  <DownloadAppButtonSmall />
+
+                </Row>
+                <Row className="footer-center">
+
+                  <Row>
+                    <Col>
+                      <h4>Quem somos</h4>
+                      <p>Sobre o App</p>
+                      <p>Sobre o Blog</p>
+                    </Col>
+                    <Col>
+                      <h4>Vantagens para clientes</h4>
+                      <p>Ofereça seus serviços</p>
+                      <p>Anuncie seus produtos</p>
+                    </Col>
+                    <Col>
+                      <h4>Localização</h4>
+                      <p>Rua Girassol, 1158 - Vila Madalena, São Paulo - SP</p>
+                      <p>Rua Girassol, 1158 - Vila Madalena, São Paulo - SP</p>
+                    </Col>
+                    <Col>
+                      <h4>Conecte-se conosco</h4>
+                      <p>(55) +11 3003-0230</p>
+                      <p>visualtotal@contato.com.br</p>
+                    </Col>
+                    <Col>
+                      <h4>Sobre a WeMind Group</h4>
+                      <p>Carreiras</p>
+                      <p>Conheça a WeMind</p>
+                    </Col>
+                  </Row>
+                  <Row>
+                    <Col>
+                      <p>Uma empresa WeMind Group - www.wemindgroup.com</p>
+                    </Col>
+                    <Col>
+                      <div className="social">
+                        <div className="facebook">
+                          <a href=""><FontAwesome
+                            name='facebook'
+                          /></a>
+                        </div>
+                        <div className="instagram">
+                          <a href=""><FontAwesome
+                            name='instagram'
+                          /></a>
+                        </div>
+                        <div className="twiter">
+                          <a href=""><FontAwesome
+                            name='twitter'
+                          /></a>
+                        </div>
+                        <div className="google-plus">
+                          <a href=""><FontAwesome
+                            name='google-plus'
+                          /></a>
+                        </div>
+                      </div>
+                    </Col>
+                    <Col>
+                      <p>Selecione um país/região</p>
+                      <DropdownButton
+                        bsStyle="default"
+                        /* TODO: must get title from state */
+                        title="Selecione um país/região"
+                        key={1}
+                        noCaret
+                      >
+                        <MenuItem id="Brazil - Português" key={1} active>Brazil - Português</MenuItem>
+                        <MenuItem id="EUA - English" key={2}>EUA - English</MenuItem>
+                      </DropdownButton>
+                    </Col>
+                  </Row>
+
+                </Row>
+                <Row className="footer-bottom">
+                  <Col>
+                  <p>© WeMind 2018 - TODOS OS DIREITOS RESERVADOS.</p>
+                  </Col>
+                </Row>
+                
               </Col>
-              <Col >
+
+              
+              {/* <Col >
                 <div className="site-links">
                   <div className="footer-heading">
                     <h4>Quem somos</h4>
@@ -84,9 +156,11 @@ export default class Footer extends Component {
                     </li>
                   </ul>
                 </div>
-              </Col>
+              </Col> */}
+
             </Row>
-            <Row>
+
+            {/* <Row>
               <Col >
                 <div className="copyright">
                   <p>© 2018 visual total</p>
@@ -100,7 +174,8 @@ export default class Footer extends Component {
                   </a>
                 </div>
               </Col>
-            </Row>
+            </Row> */}
+
           </Grid>
       </footer>
     )
