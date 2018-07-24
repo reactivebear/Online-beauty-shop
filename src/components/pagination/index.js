5d5f8deeb1c20a469d595c74179b5b5fbb1a9176
Olexandr Kovalchuk <vinni89@ukr.net>
2018-07-24 15:42:35 +0300

    Editing pagination for different screens

--- src/components/pagination/index.js
+++ src/components/pagination/index.js
@@ -12,9 +12,19 @@ class Pagination extends Component {
 		this.container = null
 	}
 
+	getHiddenClass = page => {
+		if (this.getMaxCount() < this.props.total) {
+			
+			return 'd-none'
+		}
+		return ''
+	}
+
 	printButtons = (item, i) => {
 		const activeClass = this.state.active === i+1 ? 'color-green' : 'color-grey'
-		return 	<div key={i} ref={ref => this.list[i] = ref} className={`rounded bg-white p-2 px-3 mx-1 border pointer ${activeClass}`} onClick={this.setPage(i+1)}>
+		const hiddenClass = this.getHiddenClass(i+1)
+		
+		return 	<div key={i} ref={ref => this.list[i] = ref} className={`rounded bg-white p-2 px-3 mx-1 border pointer ${activeClass} ${hiddenClass}`} onClick={this.setPage(i+1)}>
 	            	{i+1}
 	            </div>
 	}
@@ -38,6 +48,28 @@ class Pagination extends Component {
 		}
 	}
 
+	firstPage = () => {
+		this.setState({active: 1})
+		this.props.onChange(1)
+	}
+
+	lastPage = () => {
+		this.setState({active: this.props.total})
+		this.props.onChange(this.props.total)
+	}
+
+	getMaxCount = () => {
+		let count = this.props.total
+		if (this.props.responsive && this.props.responsive.length) {
+			this.props.responsive.forEach(item => {
+				if (item.width >= window.screen.availWidth) {
+					count = item.count
+				}
+			})
+		}
+		return count
+	}
+
 	componentDidMount() {
 		setTimeout(() => {
 			
@@ -57,11 +89,17 @@ class Pagination extends Component {
         return (
         	<div ref={ref => this.container = ref} style={{minHeight: 1}}>
 	        	<div className="d-flex justify-content-center">
+								<div className={`rounded bg-white py-2 px-3 mx-1 border ${prevClass}`} ref={ref => this.prev = ref} onClick={this.firstPage}>
+		            	<i className="fas fa-chevron-left"></i>
+		            </div>
 		            <div className={`rounded bg-white py-2 px-3 mx-1 border ${prevClass}`} ref={ref => this.prev = ref} onClick={this.prevPage}>
 		            	<i className="fas fa-chevron-left"></i>
 		            </div>
 		            	{ list.map((item, i) => this.printButtons(item, i)) }
 		            <div className={`rounded bg-white py-2 px-3 mx-1 border ${nextClass}`} ref={ref => this.next = ref} onClick={this.nextPage}>
+		            	<i className="fas fa-chevron-right"></i>
+		            </div>
+								<div className={`rounded bg-white py-2 px-3 mx-1 border ${nextClass}`} ref={ref => this.next = ref} onClick={this.lastPage}>
 		            	<i className="fas fa-chevron-right"></i>
 		            </div>
 				</div>
